74f74f5b30b3834b058a814cd8258e93481d0b44
AndreaLinza <andrealinza@gmail.com>
2023-09-26 18:49:59 +0200

    card hover and style

--- resources/views/partials/cardContent.blade.php
+++ resources/views/partials/cardContent.blade.php
@@ -1,10 +1,17 @@
 @foreach ($movies as $movie)
     <div class="col">
         <div class="card">
-            <img src="{{$movie->img}}" class="card-img-top" alt="...">
+            <img src="{{ $movie->img }}" class="card-img-top" alt="...">
+            <div class="hover text-center d-flex flex-column justify-content-around">
+                <h2 class="mt-5">{{ $movie->original_title }}</h2>
+                <div >
+                    <h4>Language: {{ $movie->nationality }}</h4>
+                    <h5>Vote: {{ $movie->vote }} </h5>
+                </div>
+            </div>
             <div class="card-body text-center">
-                <h4 class="card-title">{{$movie->title}}</h4>
-                <p class="card-text d-none">{{$movie->vote}}</p>
+                <h4 class="card-title">{{ $movie->title }}</h4>
+                <p class="card-text d-none">{{ $movie->vote }}</p>
             </div>
         </div>
     </div>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,7 +3,7 @@
         <a class="navbar-brand fw-bold fs-2">MovieList</a>
         <form class="d-flex input-group w-25" role="search">
             <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-warning" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            <button class="btn btn-outline-danger" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
         </form>
     </div>
 </nav>

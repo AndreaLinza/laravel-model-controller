45a4fa79b5c5e75dbf00910e0d1269178cf0dcc3
AndreaLinza <andrealinza@gmail.com>
2023-09-26 15:33:52 +0200

    first commit

---
+++ resources/views/layout/public.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+    <title>@yield("title")</title>
+
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+
+</head>
+<body>
+
+    
+    {{-- @include("partials.header")
+    @include('partials.pagesCard')
+
+    
+    @yield('homeContent')
+    @yield('comicContent')
+    @yield('charactersContent')
+    @yield('moviesContent')
+    @yield('tvContent')
+    @yield('gamesContent')
+    @yield('collectiblesContent')
+    @yield('videoContent')
+    @yield('fansContent')
+    @yield('newsContent')
+    @yield('shopContent')
+    
+    
+    @include("partials.footer") --}}
+    
+
+    
+</body>
+</html>
